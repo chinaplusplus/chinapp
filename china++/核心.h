51d4c5be838495da571ec8e800bc7bf5405d5e35
xbw <windworst@gmail.com>
2023-04-21 13:09:20 +0800

    更新: 计次循环首

--- china++/核心.h
+++ china++/核心.h
@@ -1,16 +1,18 @@
 #pragma once
 
 #define 整数 int
-#define 主函数 main
 #define 返回 return
-#define 计次循环(count) for(int 次数 = 0; 次数 < count; 次数 ++)
-#define 计次循环首(count) 计次循环(count) {
+#define 如果 if
+#define 否则 else
+#define 又如 else if
+
+#define 计次循环(variant,count) for(int variant = 0; variant < count; variant ++)
+#define 计次循环首(variant, count) 计次循环(variant, count) {
 #define 计次循环尾 }
 #define 判断循环(condition) while(condition)
 #define 判断循环首(condition) 判断循环(condition) {
 #define 判断循环尾 }
 #define 循环判断首 do{
 #define 循环判断尾(condition) } while(condition);
-#define 如果 if
-#define 否则 else
-#define 又如 else if
+
+#define 主函数 main
