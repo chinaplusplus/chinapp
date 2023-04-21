99361dbe21ba782c1e754df3bcf5281ac0ad62e9
xbw <windworst@gmail.com>
2023-04-21 12:33:29 +0800

    初始化仓库

---
+++ china++/扩展/输入输出.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <stdio.h>
+
+#define 打印 printf
---
+++ china++/核心.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#define 整数 int
+#define 主函数 main
+#define 返回 return
+#define 计次循环(count) for(int 次数 = 0; 次数 < count; 次数 ++)
+#define 计次循环首(count) 计次循环(count) {
+#define 计次循环尾 }
+#define 判断循环(condition) while(condition)
+#define 判断循环首(condition) 判断循环(condition) {
+#define 判断循环尾 }
+#define 循环判断首 do{
+#define 循环判断尾(condition) } while(condition);
+#define 如果 if
+#define 否则 else
+#define 又如 else if
